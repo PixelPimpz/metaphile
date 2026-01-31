22d335a3716fd763e991fa7d0fa04626a1a2b315
lucifer-morningstar <slobeck@gmail.com>
2026-01-30 18:35:02 -0800

    Generic commit

--- metaphile.tmux
+++ metaphile.tmux
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Welcome, adventurer!"
-tmux setenv -u @PLUG_ROOT
-tmux setenv '@PLUG_ROOT' "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PLUG_ROOT="$(tmux display -p "#{@PLUG_ROOT}")"
-ICONS="$(tmux display -p "#{@LIB_ICON}")"
-tmux bind M-m run "$PLUG_ROOT/tmux-metaphile"
-
-## run main plugin script
-$PLUG_ROOT/scripts/tmux-metaphile.sh
+LOCAL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ICONS="$(tmux show -gqv @ICONS)"
+SHARE="$( tmux show -gqv @CHER )"
+source "$SHARE/dump.fun"
+source "$SHARE/fatal.fun"
+dump ">>> welcome adventurer!"
+tmux bind M-m run-shell "$LOCAL_ROOT/scripts/metaphile.sh"
+tmux run-shell "$LOCAL_ROOT/scripts/metaphile.sh"
--- scripts/metaphile.sh
+++ scripts/metaphile.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-#tmux display -p "tmux-metaphile.sh running..."
-tmux source-file '$TMUX_ROOT/tmux.conf'
+# -----------------------------------------------------------------------------
+# first gather context info: are we in ther shell, neovim or something else?
+#   if not in nvim, gather the current shell command and the pwd with 
+#   "tmux pane-current-path" 
+#   put the current-command's icon and the cwd into section 1.1
+#   put the current-command and it's icon into section 1.2
+# if nvim is running and the open buffer is in a directory tree with a ".git"
+# folder at its root, then run gitInfo and put the remote repo and a github 
+# icon in section 1.1 and the NeoVim logo icon + the file name in section 1.2
+# add completed sections to somewhere
+# -----------------------------------------------------------------------------
+LOCAL_ROOT=$( tmux display -p #{pane_current_path} )
+SHARE=$( tmux show -gqv @CHER )
+source "$SHARE/dump.fun"
+source "$SHARE/fatal.fun"
+dump ">>> metaphile.sh running..."
+dump ">> $LOCAL_ROOT"
