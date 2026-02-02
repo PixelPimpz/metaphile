92e854c3d65b2ab9abfb5eef473fbcaa53ecbf3e
lucifer-morningstar <slobeck@gmail.com>
2026-02-02 08:07:22 -0800

    Generic commit

--- scripts/metaphile.sh
+++ scripts/metaphile.sh
@@ -27,7 +27,7 @@ main()
   local PARENT_PROC="$(ps -q "${PANE_PID}" o comm=)"
 
   if [[ "${SOCKET}" =~ ${PANE_PID} ]]; then # /tmp/nvim-XXXXX = nvim ... /tmp/ = no nvim socket 
-    local ICON="$( yaml2item ".icons.app.Folder" $ICONS )"
+    local ICON="$( yaml2item ".icons.sys.Folder" $ICONS )"
     local BUF_NAME="$( nvim --server ${SOCKET} --remote-expr 'expand("%:t")' )"
   else
     local ICON="$( yaml2item ".icons.app.Folder" $ICONS )"
