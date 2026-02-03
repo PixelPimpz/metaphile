86107998fd08988a6b483a9419db9672960bb41a
lucifer-morningstar <slobeck@gmail.com>
2026-02-02 18:07:00 -0800

    Generic commit

--- scripts/metaphile.sh
+++ scripts/metaphile.sh
@@ -22,7 +22,7 @@ main()
 {
   tmux set -g '@MF_NAME' "$(mf_name)"
   tmux set -g '@MF_GIT' "$(mf_git)"
-  ps -q $(tmux display -p "#{pane_pid}" o comm=)
+  ps -q $(tmux display -p "#{pane_pid}") o comm=
 }
 
 mf_git()
