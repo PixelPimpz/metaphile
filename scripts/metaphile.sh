7f548546df8029f056c4e8f73f037af4e9d8122c
lucifer-morningstar <slobeck@gmail.com>
2026-02-04 22:48:52 -0800

    Generic commit

--- scripts/metaphile.sh
+++ scripts/metaphile.sh
@@ -26,7 +26,7 @@ main()
   tmux set -g '@MF_NAME' "${mf_path_array[0]} ${mf_path_array[1]##*/}"
   tmux set -g '@MF_PATH' "${mf_path_array[1]}"
   tmux set -g '@MF_GIT' "$(mf_git)"
-  dump  "${mf_path_array[1]}"
+  dump  "${mf_path_array[1]%/*}"
   #git ls-files --error-unmatch "${mf_path_array[1]}"
 }
  
