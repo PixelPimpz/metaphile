6008d03d9e58a6070a5a98e4cca7a70e7bc9501c
lucifer-morningstar <slobeck@gmail.com>
2026-02-05 05:05:34 -0800

    Generic commit

--- scripts/metaphile.sh
+++ scripts/metaphile.sh
@@ -38,7 +38,7 @@ main()
 mf_git()
 {
   local gitroot=$1
-  local repo="$( git -C $gitroot info | grep -E "\(push\)" | sed 's/^.*://;s/ .*$//' )"
+  local repo="$( git -C $gitroot info | grep -E "\(push\)" | sed 's/^.*://;s/\.git .*$//' )"
   local ICON=$( yaml2item ".icons.app.gh" $ICONS )
   echo "${ICON} $repo"
 }
